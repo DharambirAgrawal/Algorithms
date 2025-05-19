a955040bedca774f1e357e174473fc852f721ef8
Dharambir Agrawal <dev.dharambir@gmail.com>
2025-05-19 19:29:07 +0000

    Added the req formula

--- searching/algorithms.py
+++ searching/algorithms.py
@@ -3,3 +3,13 @@
 class Searching:
     def __init__(self,data, visualizer):
         self.array = data
+
+    def traditional_binary_search(self,target):
+
+        """
+        M = (L+R) // 2  
+        or
+        M = L + (R-L) // 2  --> prevents overflow
+        """
+
+
